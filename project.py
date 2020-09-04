e6dd91af19600c2843fa91e744fa4d2a90a95bb2
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-09-04 19:09:55 +0430

    Booleans and Compaisons

--- project.py
+++ project.py
@@ -1,7 +1,20 @@
-x=2
-print(x)
-x+=3
-print(x)
-t="Hello"
-t+=" world!"
-print(t)
+boolean=True
+print(boolean)
+print(2==3)
+#False
+
+print("Hello"=="Hello")
+#True
+
+print(1 != 1)
+#False
+
+print(7>5)
+#True
+print(10<10)
+#False
+
+print(7 <= 8)
+#True
+print(9>=9.0)
+#True
