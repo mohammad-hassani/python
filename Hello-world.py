b4bdb01732cdd92db583baa95773ff984d2c845a
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-09-04 18:37:32 +0430

    String

--- Hello-world.py
+++ Hello-world.py
@@ -1,3 +1,6 @@
-print((1+3)**2)
-print(20//6)
-print(20%6)
+print("python is fun!")
+print('Brain\'s mother : He\'s not an angel. He\'s a very naughty boy!')
+print("""this
+is a
+multiline
+text""")
