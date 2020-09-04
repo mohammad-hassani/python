9152f03f0f1cc674e94205b9d88097dc94ebcde5
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-09-04 19:23:54 +0430

    Boolean Logic

--- project.py
+++ project.py
@@ -1,38 +1,14 @@
-if 10>5:
-    print("10 is grater than 5")
-print("end program")
+print(1==1 and 2==2)
+#true
+print(1==1 and 2==3)
+#false
 
+print(1!=1 or 2==2)
+#true
+print(2<1 or 3>6)
+#false
 
-num=12
-if num>5:
-    print("bigger than 5")
-    if num <= 47:
-        print('between 5 and 47')
-
-x=4
-if x ==5:
-    print("yes")
-else:
-    print("no")
-
-n=3
-if n==1:
-    print("one")
-else:
-    if n==2:
-        print("two")
-    else:
-        if n==3:
-            print("three")
-        else:
-            print("somthing else")
-
-
-if n==1:
-    print("one")
-elif n==2:
-    print("two")
-elif n==3:
-    print("three")
-else:
-    print("somthing else")                                            
+print(not 1==1)
+#false
+print(not 1>7)
+#true
