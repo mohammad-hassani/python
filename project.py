bd198ded9e0fef897197b4a138ce64c897aacebf
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-09-04 19:18:46 +0430

    else and elif Statements

--- project.py
+++ project.py
@@ -8,3 +8,31 @@ if num>5:
     print("bigger than 5")
     if num <= 47:
         print('between 5 and 47')
+
+x=4
+if x ==5:
+    print("yes")
+else:
+    print("no")
+
+n=3
+if n==1:
+    print("one")
+else:
+    if n==2:
+        print("two")
+    else:
+        if n==3:
+            print("three")
+        else:
+            print("somthing else")
+
+
+if n==1:
+    print("one")
+elif n==2:
+    print("two")
+elif n==3:
+    print("three")
+else:
+    print("somthing else")                                            
