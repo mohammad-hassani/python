5a8d0a5e4c3974d5491e1d3b955de71dc010ef11
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2020-09-04 18:44:10 +0430

    variables

--- Hello-world.py
+++
@@ -1,6 +0,0 @@
-print("python is fun!")
-print('Brain\'s mother : He\'s not an angel. He\'s a very naughty boy!')
-print("""this
-is a
-multiline
-text""")
---
+++ project.py
@@ -0,0 +1,10 @@
+user ="ali"
+x=7
+print(x)
+print(user)
+print(x+3)
+print(user + " alavi")
+foo=3
+del foo
+foo =4
+print(foo)
